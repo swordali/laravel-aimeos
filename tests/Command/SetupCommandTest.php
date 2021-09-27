2580f5ad19800ecb9e903eb6fc6c57460c0bcbe3
Ali Kılıç <swordali@gmail.com>
2021-09-27 20:44:30 +0300

    Initialize 1.1

---
+++ tests/Command/SetupCommandTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class SetupCommandTest extends AimeosTestAbstract
+{
+	public function testSetupCommand()
+	{
+		$params = array('--option' => 'setup/default/demo:0');
+		$this->assertEquals(0, $this->artisan('aimeos:setup', array('site' => 'unittest'), $params));
+	}
+}
