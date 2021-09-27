67f6a09301d6930815069a312303ef1a99e9485c
Ali Kılıç <swordali@gmail.com>
2021-09-27 20:44:53 +0300

    Initialize 2019.10

---
+++ tests/Base/SupportTest.php
@@ -0,0 +1,40 @@
+<?php
+
+class SupportTest extends AimeosTestAbstract
+{
+	public function testCheckUserGroup()
+	{
+		$context = $this->app->make( '\Aimeos\Shop\Base\Context' );
+		$locale = $this->app->make( '\Aimeos\Shop\Base\Locale' );
+
+		$object = new \Aimeos\Shop\Base\Support( $context, $locale );
+		$user = new \Illuminate\Foundation\Auth\User();
+
+		$this->assertFalse( $object->checkUserGroup( $user, 'admin' ) );
+	}
+
+
+	public function testCheckGroup()
+	{
+		$context = $this->app->make( '\Aimeos\Shop\Base\Context' );
+		$locale = $this->app->make( '\Aimeos\Shop\Base\Locale' );
+
+		$object = new \Aimeos\Shop\Base\Support( $context, $locale );
+
+		$this->assertFalse( $object->checkGroup( -1, 'admin' ) );
+	}
+
+
+	public function testGetGroups()
+	{
+		$context = $this->app->make( '\Aimeos\Shop\Base\Context' );
+		$locale = $this->app->make( '\Aimeos\Shop\Base\Locale' );
+
+		$ctx = $context->get( false );
+		$ctx->setLocale( $locale->getBackend( $ctx, 'unittest' ) );
+
+		$object = new \Aimeos\Shop\Base\Support( $context, $locale );
+
+		$this->assertEquals( array(), $object->getGroups( $ctx ) );
+	}
+}
