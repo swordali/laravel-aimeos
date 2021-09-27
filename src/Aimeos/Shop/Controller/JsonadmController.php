679151207c283f2606c92fd526db2452c51d449c
Ali Kılıç <swordali@gmail.com>
2021-09-27 20:44:41 +0300

    Initialize 2018.04

---
+++ src/Aimeos/Shop/Controller/JsonadmController.php
@@ -0,0 +1,150 @@
+<?php
+
+/**
+ * @license MIT, http://opensource.org/licenses/MIT
+ * @copyright Aimeos (aimeos.org), 2015-2016
+ * @package laravel
+ * @subpackage Controller
+ */
+
+
+namespace Aimeos\Shop\Controller;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response;
+
+
+/**
+ * Aimeos controller for the JSON REST API
+ *
+ * @package laravel
+ * @subpackage Controller
+ */
+class JsonadmController extends Controller
+{
+	use AuthorizesRequests;
+
+
+	/**
+	 * Deletes the resource object or a list of resource objects
+	 *
+	 * @param \Psr\Http\Message\ServerRequestInterface $request Request object
+	 * @return \Psr\Http\Message\ResponseInterface Response object containing the generated output
+	 */
+	public function deleteAction( ServerRequestInterface $request )
+	{
+		if( config( 'shop.authorize', true ) ) {
+			$this->authorize( 'admin', [JsonadmController::class, ['admin', 'api']] );
+		}
+
+		return $this->createClient()->delete( $request, new Response() );
+	}
+
+
+	/**
+	 * Returns the requested resource object or list of resource objects
+	 *
+	 * @param \Psr\Http\Message\ServerRequestInterface $request Request object
+	 * @return \Psr\Http\Message\ResponseInterface Response object containing the generated output
+	 */
+	public function getAction( ServerRequestInterface $request )
+	{
+		if( config( 'shop.authorize', true ) ) {
+			$this->authorize( 'admin', [JsonadmController::class, ['admin', 'api', 'editor']] );
+		}
+
+		return $this->createClient()->get( $request, new Response() );
+	}
+
+
+	/**
+	 * Updates a resource object or a list of resource objects
+	 *
+	 * @param \Psr\Http\Message\ServerRequestInterface $request Request object
+	 * @return \Psr\Http\Message\ResponseInterface Response object containing the generated output
+	 */
+	public function patchAction( ServerRequestInterface $request )
+	{
+		if( config( 'shop.authorize', true ) ) {
+			$this->authorize( 'admin', [JsonadmController::class, ['admin', 'api']] );
+		}
+
+		return $this->createClient()->patch( $request, new Response() );
+	}
+
+
+	/**
+	 * Creates a new resource object or a list of resource objects
+	 *
+	 * @param \Psr\Http\Message\ServerRequestInterface $request Request object
+	 * @return \Psr\Http\Message\ResponseInterface Response object containing the generated output
+	 */
+	public function postAction( ServerRequestInterface $request )
+	{
+		if( config( 'shop.authorize', true ) ) {
+			$this->authorize( 'admin', [JsonadmController::class, ['admin', 'api']] );
+		}
+
+		return $this->createClient()->post( $request, new Response() );
+	}
+
+
+	/**
+	 * Creates or updates a single resource object
+	 *
+	 * @param \Psr\Http\Message\ServerRequestInterface $request Request object
+	 * @return \Psr\Http\Message\ResponseInterface Response object containing the generated output
+	 */
+	public function putAction( ServerRequestInterface $request )
+	{
+		if( config( 'shop.authorize', true ) ) {
+			$this->authorize( 'admin', [JsonadmController::class, ['admin', 'api']] );
+		}
+
+		return $this->createClient()->put( $request, new Response() );
+	}
+
+
+	/**
+	 * Returns the available HTTP verbs and the resource URLs
+	 *
+	 * @param \Psr\Http\Message\ServerRequestInterface $request Request object
+	 * @return \Psr\Http\Message\ResponseInterface Response object containing the generated output
+	 */
+	public function optionsAction( ServerRequestInterface $request )
+	{
+		if( config( 'shop.authorize', true ) ) {
+			$this->authorize( 'admin', [JsonadmController::class, ['admin', 'api', 'editor']] );
+		}
+
+		return $this->createClient()->options( $request, new Response() );
+	}
+
+
+	/**
+	 * Returns the JsonAdm client
+	 *
+	 * @return \Aimeos\Admin\JsonAdm\Iface JsonAdm client
+	 */
+	protected function createClient()
+	{
+		$site = Route::input( 'site', Input::get( 'site', 'default' ) );
+		$lang = Input::get( 'locale', config( 'app.locale', 'en' ) );
+		$resource = Route::input( 'resource' );
+
+		$aimeos = app( '\Aimeos\Shop\Base\Aimeos' )->get();
+		$templatePaths = $aimeos->getCustomPaths( 'admin/jsonadm/templates' );
+
+		$context = app( '\Aimeos\Shop\Base\Context' )->get( false, 'backend' );
+		$context->setI18n( app('\Aimeos\Shop\Base\I18n')->get( array( $lang, 'en' ) ) );
+		$context->setLocale( app('\Aimeos\Shop\Base\Locale')->getBackend( $context, $site ) );
+		$context->setView( app( '\Aimeos\Shop\Base\View' )->create( $context, $templatePaths, $lang ) );
+
+		return \Aimeos\Admin\JsonAdm\Factory::createClient( $context, $aimeos, $resource );
+	}
+}
---
+++ src/Aimeos/Shop/ShopServiceProvider.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * @license MIT, http://opensource.org/licenses/MIT
+ * @copyright Aimeos (aimeos.org), 2015-2016
+ * @package laravel
+ */
+
+
+namespace Aimeos\Shop;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Route;
+
+
+/**
+ * Aimeos shop service provider for Laravel
+ * @package laravel
+ */
+class ShopServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$ds = DIRECTORY_SEPARATOR;
+		$basedir = dirname( dirname( __DIR__ ) ) . $ds;
+
+		$this->loadRoutesFrom( $basedir . 'routes.php' );
+		$this->loadViewsFrom( $basedir . 'views', 'shop' );
+
+		$this->publishes( [ $basedir . 'config/shop.php' => config_path( 'shop.php' ) ], 'config' );
+		$this->publishes( [ dirname( $basedir ) . $ds . 'public' => public_path( 'packages/aimeos/shop' ) ], 'public' );
+	}
+
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->mergeConfigFrom( dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR . 'default.php', 'shop');
+
+		$this->app->singleton('Aimeos\Shop\Base\Aimeos', function($app) {
+			return new \Aimeos\Shop\Base\Aimeos($app['config']);
+		});
+
+		$this->app->singleton('Aimeos\Shop\Base\Config', function($app) {
+			return new \Aimeos\Shop\Base\Config($app['config'], $app['Aimeos\Shop\Base\Aimeos']);
+		});
+
+		$this->app->singleton('Aimeos\Shop\Base\I18n', function($app) {
+			return new \Aimeos\Shop\Base\I18n($this->app['config'], $app['Aimeos\Shop\Base\Aimeos']);
+		});
+
+		$this->app->singleton('Aimeos\Shop\Base\Locale', function($app) {
+			return new \Aimeos\Shop\Base\Locale($app['config']);
+		});
+
+		$this->app->singleton('Aimeos\Shop\Base\Context', function($app) {
+			return new \Aimeos\Shop\Base\Context($app['session.store'], $app['Aimeos\Shop\Base\Config'], $app['Aimeos\Shop\Base\Locale'], $app['Aimeos\Shop\Base\I18n']);
+		});
+
+		$this->app->singleton('Aimeos\Shop\Base\Page', function($app) {
+			return new \Aimeos\Shop\Base\Page($app['config'], $app['Aimeos\Shop\Base\Aimeos'], $app['Aimeos\Shop\Base\Context'], $app['Aimeos\Shop\Base\Locale'], $app['Aimeos\Shop\Base\View']);
+		});
+
+		$this->app->singleton('Aimeos\Shop\Base\Support', function($app) {
+			return new \Aimeos\Shop\Base\Support($app['Aimeos\Shop\Base\Context'], $app['Aimeos\Shop\Base\Locale']);
+		});
+
+		$this->app->singleton('Aimeos\Shop\Base\View', function($app) {
+			return new \Aimeos\Shop\Base\View($app['config'], $app['Aimeos\Shop\Base\I18n'], $app['Aimeos\Shop\Base\Support']);
+		});
+
+
+		$this->commands( array(
+			'Aimeos\Shop\Command\AccountCommand',
+			'Aimeos\Shop\Command\CacheCommand',
+			'Aimeos\Shop\Command\SetupCommand',
+			'Aimeos\Shop\Command\JobsCommand',
+		) );
+	}
+
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array(
+			'Aimeos\Shop\Base\Aimeos', 'Aimeos\Shop\Base\I18n', 'Aimeos\Shop\Base\Context',
+			'Aimeos\Shop\Base\Config', 'Aimeos\Shop\Base\Locale', 'Aimeos\Shop\Base\View',
+			'Aimeos\Shop\Base\Page', 'Aimeos\Shop\Base\Support',
+			'Aimeos\Shop\Command\AccountCommand', 'Aimeos\Shop\Command\CacheCommand',
+			'Aimeos\Shop\Command\SetupCommand', 'Aimeos\Shop\Command\JobsCommand',
+		);
+	}
+
+}
